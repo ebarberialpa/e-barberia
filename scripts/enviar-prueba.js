a39a971a05f13913d2b84b8ed905f432146257d7
ebarberialpa <ebarberialpa@gmail.com>
2026-07-24 13:26:55 +0100

    Update GMAIL_APP_PASSWORD to GMAIL_PASSWORD

--- scripts/enviar-prueba.js
+++ scripts/enviar-prueba.js
@@ -31,7 +31,7 @@ const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
         user: process.env.GMAIL_USER,
-        pass: process.env.GMAIL_APP_PASSWORD
+        pass: process.env.GMAIL_PASSWORD
     }
 });
 
